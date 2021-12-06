604cfe0b14fc3a851d579998c1a342dd5dd31b29
Abdallah Ehab <ehab.abdallah65@gmail.com>
2021-12-06 22:32:51 +0200

    phone verification fragment done

---
+++ app/src/main/java/com/example/android/nadris/phoneVerification/PhoneViewModelFactory.kt
@@ -0,0 +1,19 @@
+package com.example.android.nadris.phoneVerification
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class PhoneViewModelFactory(private val receivedOTB:String):ViewModelProvider.Factory {
+
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+
+        @Suppress("unchecked_cast")
+        if(modelClass.isAssignableFrom(PhoneViewModel::class.java)){
+            return PhoneViewModel(receivedOTB) as T
+        }
+                throw IllegalArgumentException("Unknown ViewModel class")
+    }
+
+
+}
