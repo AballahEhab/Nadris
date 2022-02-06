31a394ecb72668941a03a075cabcb39070a6f92f
abdallahGhorap1212 <abdallah_salah201616@yahoo.com>
2022-02-06 05:17:34 +0200

    اخر تعديل علي ال post page

--- app/src/main/java/com/example/android/nadris/AddPostViewModel.kt
+++ app/src/main/java/com/example/android/nadris/AddPostViewModel.kt
@@ -1,10 +1,27 @@
 package com.example.android.nadris
 
+import android.content.Intent
+import android.widget.ImageView
+import androidx.core.app.ActivityCompat.startActivityForResult
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-var questionList= mutableListOf<AddPostViewModel>()
+
+
 class AddPostViewModel : ViewModel() {
     var subjects : MutableLiveData<String> = MutableLiveData<String>()
     var question : MutableLiveData<String> = MutableLiveData<String>()
-    val id : Int?= questionList.size
+    var IMAGE_REQUEST_CODE=456
+    var READ_STORAGE_PERM = 123
+    var image : Int = 0
+
+
+
+
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/android/nadris/addPostFragment.kt
+++ app/src/main/java/com/example/android/nadris/addPostFragment.kt
@@ -1,14 +1,26 @@
 package com.example.android.nadris
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.android.nadris.databinding.AddPostFragmentBinding
 import com.example.android.nadris.databinding.PostPageFragmentBinding
 import com.example.android.nadris.databinding.SignupStudentFragmentBinding
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
+import android.content.pm.PackageManager as PmPackageManager
 
 class addPostFragment : Fragment() {
 
@@ -26,6 +38,78 @@ class addPostFragment : Fragment() {
         inflater.inflate(R.layout.add_post_fragment, container, false)
         val bindigin =AddPostFragmentBinding.inflate(inflater)
         bindigin.addPostViewModel = viewModel
+        val subjects = resources.getStringArray(R.array.subject)
+        val adapter= ArrayAdapter(requireContext(), R.layout.list_item, subjects)
+        (bindigin.spAddSubject.editText as? AutoCompleteTextView)?.setAdapter(adapter)!!
+        bindigin.imageButton.setOnClickListener{
+
+            pickImageFromGallery()
+
+ }
+        bindigin.publicButton.setOnClickListener {
+           var str:String=""+viewModel.question.value+"\n"+viewModel.subjects.value+"\n"+viewModel.image;
+          Toast.makeText(requireContext(),str,Toast.LENGTH_LONG).show()
+       }
+
         return bindigin.root}
+
+
+     fun pickImageFromGallery() {
+         val intent = Intent(Intent.ACTION_PICK)
+       intent.type = "image/*"
+         startActivityForResult(intent, viewModel.IMAGE_REQUEST_CODE) }
+
+    // edit to imageview
+
+    // override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+     //   super.onActivityResult(requestCode, resultCode, data)
+      // if (requestCode == viewModel.IMAGE_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+       //     binding.imageView.setImageURI(data?.data)
+       // }
+
+
+    /*   take photo from camera
+    to do
+     */
+
+    //private fun hasReadStoragePerm():Boolean{
+     //   return EasyPermissions.hasPermissions(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+   // }
+   // private fun readStorageTask(){
+     //   if (hasReadStoragePerm()){
+
+
+       //     pickImageFromGallery()
+       // }else{
+         //   EasyPermissions.requestPermissions(
+          //      requireActivity(),
+            //    getString(R.string.storage_permission_text),
+             //   viewModel.READ_STORAGE_PERM,
+              //  Manifest.permission.READ_EXTERNAL_STORAGE
+          //  )
+        //}
+    //}
+   // private fun pickImageFromGallery(){
+     //   var intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+     //   if (intent.resolveActivity(requireActivity().packageManager) != null){
+      //      startActivityForResult(intent,viewModel.IMAGE_REQUEST_CODE)
+     //   }
+   // }
+  //  fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+     //  if (EasyPermissions.somePermissionPermanentlyDenied(requireActivity(),perms)){
+       //     AppSettingsDialog.Builder(requireActivity()).build().show()
+     //   }
+   // }
+    // fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+
+   // }
+
+     //fun onRationaleDenied(requestCode: Int) {
+
+   // }
+
+     //fun onRationaleAccepted(requestCode: Int) {
+
+   // }
 
 }
