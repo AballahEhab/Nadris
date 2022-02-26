1a629f99c5c79170d99b2cae31b8d3df815813df
Abdallah Ehab <ehab.abdallah65@gmail.com>
2022-02-27 00:50:22 +0200

    updating database calsses

--- app/src/main/java/com/example/android/nadris/database/NadrisDatabase.kt
+++ app/src/main/java/com/example/android/nadris/database/NadrisDatabase.kt
@@ -4,12 +4,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.android.nadris.database.dao.PostDao
 import com.example.android.nadris.database.dao.UserDao
+import com.example.android.nadris.database.models.*
 import com.example.android.nadris.domain.CommentData
-import com.example.android.nadris.database.models.DatabasePost
-import com.example.android.nadris.database.models.UserData
 
 
-@Database(entities = [UserData::class, DatabasePost::class, CommentData::class], version = 1, exportSchema = false)
+@Database(entities = [UserData::class, DatabasePost::class, DatabaseSubject::class, Lesson::class, SubjectUnit::class], version = 1, exportSchema = false)
 abstract class NadrisDatabase : RoomDatabase() {
 
     abstract fun UserDao(): UserDao
